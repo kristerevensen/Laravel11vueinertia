11994b6fb4d81fb28d3eb098e4bbba75f1a2498e
kristerevensen <67630525+kristerevensen@users.noreply.github.com>
2024-07-29 23:53:47 +0200

    29 jul 2024

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="flex justify-between p-4 bg-gray-200">
         <div class="text-lg">
-           Navbar
+           Campaign Manager
         </div>
         <div>
             <ul class="flex gap-x-4 me-auto mb-2 mb-lg-0">
